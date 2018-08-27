b59ce9254a6ca6e6ba311132773a6a717913d5a1
SERGHEI GURAGAT <serghei.guragat@yahoo.com>
2018-08-27 21:41:54 +0100

    Overload methods in Utils class

--- src/main/java/com/vulcan/common/Utils.java
+++ src/main/java/com/vulcan/common/Utils.java
@@ -3,6 +3,8 @@ package com.vulcan.common;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 /**
  * Created by sg on 29/07/2018.
  */
@@ -10,8 +12,16 @@ public class Utils {
 
     public final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+    public static String asJson(Object pojo) {
+        return asJson(pojo, OBJECT_MAPPER, false);
+    }
+
     public static String asJson(Object pojo, ObjectMapper objectMapper) {
         return asJson(pojo, objectMapper, false);
+    }
+
+    public static String asJson(Object pojo, boolean pretty) {
+        return asJson(pojo, OBJECT_MAPPER, pretty);
     }
 
     public static String asJson(Object pojo, ObjectMapper objectMapper, boolean pretty) {
@@ -21,7 +31,19 @@ public class Utils {
             }
             return objectMapper.writeValueAsString(pojo);
         } catch (JsonProcessingException e) {
-            throw new IllegalStateException(String.format("Failed to convert POJO (%s >> %s) into Json-String", pojo.getClass().getName(), pojo), e);
+            throw new IllegalStateException(String.format("Failed to convert POJO (%s, %s) into JSON-String", pojo.getClass().getName(), pojo), e);
+        }
+    }
+
+    public static <E> E asPojo(Class<E> pojoType, String json) {
+        return asPojo(pojoType, json, OBJECT_MAPPER);
+    }
+
+    public static <E> E asPojo(Class<E> pojoType, String json, ObjectMapper objectMapper) {
+        try {
+            return objectMapper.readValue(json, pojoType);
+        } catch (IOException e) {
+            throw new IllegalStateException(String.format("Failed to convert JSON-String (%s) into POJO (%s)", json, pojoType), e);
         }
     }
 }
